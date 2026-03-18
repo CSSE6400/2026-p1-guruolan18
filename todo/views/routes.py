5c52126ae73c2f4cdd6efa9e147a36f15c2f2560
guruolan <guruolan18@gmail.com>
2026-03-18 20:57:34 +1000

    添加了create_todo()

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -17,6 +17,19 @@ def get_todos():
         "created_at": "2026-02-20T14:00:00",
         "updated_at": "2026-02-20T14:00:00"
     }])
+
+@api.route('/todos', methods=['POST'])
+def create_todo():
+    return jsonify({
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2026-02-27T18:00:00",
+        "created_at": "2026-02-20T14:00:00",
+        "updated_at": "2026-02-20T14:00:00"
+    }), 201  # 201 = Created
+
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
     return jsonify({
